a0c3ca5b1084f23a4b1091137d5559afbf04c1e1
unknown <3758302+goatgoose@users.noreply.github.com>
2023-03-18 16:31:38 -0400

    masking

--- static/scripts/catan.js
+++ static/scripts/catan.js
@@ -7,8 +7,20 @@ app.renderer.view.style.display = "block";
 app.renderer.autoDensity = true;
 app.resizeTo = window;
 
-function updateUIHeight(event) {
-    document.getElementById("ui-container").style.height = window.innerHeight + "px";
-}
-window.addEventListener("resize", updateUIHeight);
-window.addEventListener("load", updateUIHeight);
+const Application = PIXI.Application,
+    Sprite = PIXI.Sprite,
+    Container = PIXI.Container,
+    Graphics = PIXI.Graphics
+
+let wheat_tile = Sprite.from("/static/assets/catan/tile_grain-resources.assets-684.png");
+let mask = new Graphics();
+mask.beginFill(0xffffff);
+mask.drawRoundedPolygon(255, 255, 210, 6, 10)
+mask.endFill();
+
+let container = new Container();
+container.mask = mask;
+container.addChild(wheat_tile);
+container.position.set(0, 0);
+app.stage.addChild(container);
+
