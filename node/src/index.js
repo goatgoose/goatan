6a08ccd6522d46ab99945aead5981f1a3810ed96
unknown <3758302+goatgoose@users.noreply.github.com>
2023-03-19 11:42:13 -0400

    fix sidebar

--- node/src/index.js
+++ node/src/index.js
@@ -1,9 +1,17 @@
 import * as PIXI from 'pixi.js'
 import { Graphics } from '@pixi/graphics';
+import '@pixi/graphics-extras';
 import { Viewport } from 'pixi-viewport'
 
-const app = new PIXI.Application()
-document.body.appendChild(app.view)
+let canvas_container = document.getElementById("canvas-container");
+
+const app = new PIXI.Application();
+canvas_container.appendChild(app.view);
+
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+//app.renderer.autoDensity = true;
+app.resizeTo = canvas_container;
 
 // create viewport
 const viewport = new Viewport({
@@ -24,6 +32,18 @@ viewport
     .pinch()
     .wheel()
     .decelerate()
+
+// let wheat_tile = PIXI.Sprite.from("/static/assets/catan/tile_grain-resources.assets-684.png");
+// let mask = new Graphics();
+// mask.beginFill(0xffffff);
+// mask.drawRoundedPolygon(255, 255, 210, 6, 10)
+// mask.endFill();
+//
+// let container = new PIXI.Container();
+// container.mask = mask;
+// container.addChild(wheat_tile);
+// container.position.set(0, 0);
+// viewport.addChild(container);
 
 // add a red box
 const sprite = viewport.addChild(new PIXI.Sprite(PIXI.Texture.WHITE))
