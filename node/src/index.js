1288c000480302397326a712bf9b848683188304
unknown <3758302+goatgoose@users.noreply.github.com>
2023-03-19 11:48:25 -0400

    catan tile scrolling

--- node/src/index.js
+++ node/src/index.js
@@ -13,40 +13,31 @@ app.renderer.view.style.display = "block";
 //app.renderer.autoDensity = true;
 app.resizeTo = canvas_container;
 
-// create viewport
 const viewport = new Viewport({
     screenWidth: window.innerWidth,
     screenHeight: window.innerHeight,
     worldWidth: 1000,
     worldHeight: 1000,
 
-    events: app.renderer.events // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+    events: app.renderer.events
 })
-
-// add the viewport to the stage
 app.stage.addChild(viewport)
 
-// activate plugins
 viewport
     .drag()
     .pinch()
     .wheel()
     .decelerate()
 
-// let wheat_tile = PIXI.Sprite.from("/static/assets/catan/tile_grain-resources.assets-684.png");
-// let mask = new Graphics();
-// mask.beginFill(0xffffff);
-// mask.drawRoundedPolygon(255, 255, 210, 6, 10)
-// mask.endFill();
-//
-// let container = new PIXI.Container();
-// container.mask = mask;
-// container.addChild(wheat_tile);
-// container.position.set(0, 0);
-// viewport.addChild(container);
+let wheat_tile = PIXI.Sprite.from("/static/assets/catan/tile_grain-resources.assets-684.png");
+let mask = new Graphics();
+mask.beginFill(0xffffff);
+mask.drawRoundedPolygon(255, 255, 210, 6, 10)
+mask.endFill();
+viewport.addChild(mask);
 
-// add a red box
-const sprite = viewport.addChild(new PIXI.Sprite(PIXI.Texture.WHITE))
-sprite.tint = 0xff0000
-sprite.width = sprite.height = 100
-sprite.position.set(100, 100)
+let container = new PIXI.Container();
+container.mask = mask;
+container.addChild(wheat_tile);
+container.position.set(0, 0);
+viewport.addChild(container);
