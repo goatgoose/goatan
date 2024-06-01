f9d546ed9d6abf5fd5c14a4c34e74b2913a8f849
Sam Clark <3758302+goatgoose@users.noreply.github.com>
2024-06-01 12:13:17 -0400

    draw from spritesheet

--- node/src/index.js
+++ node/src/index.js
@@ -5,6 +5,7 @@ import {Viewport} from 'pixi-viewport'
 import arrayShuffle from 'array-shuffle'
 
 import {standard_board_definition, standard_tile_set} from './standard_board'
+import {Assets, Loader, Sprite, Spritesheet, Texture} from "pixi.js";
 
 let canvas_container = document.getElementById("canvas-container");
 
@@ -16,11 +17,19 @@ app.renderer.view.style.display = "block";
 //app.renderer.autoDensity = true;
 app.resizeTo = canvas_container;
 
+// https://github.com/loksland/pixel-art-game-test/blob/main/README.md
+PIXI.BaseTexture.defaultOptions.scaleMode = PIXI.SCALE_MODES.NEAREST;
+app.view.style.imageRendering = 'pixelated';
+const PIXEL_SCALE = 1;
+app.stage.scale.set(PIXEL_SCALE, PIXEL_SCALE);
+
+const WORLD_WIDTH = 200;
+const WORLD_HEIGHT = 200;
 const viewport = new Viewport({
     screenWidth: window.innerWidth,
     screenHeight: window.innerHeight,
-    worldWidth: 1000,
-    worldHeight: 1000,
+    worldWidth: WORLD_WIDTH,
+    worldHeight: WORLD_HEIGHT,
 
     events: app.renderer.events
 })
@@ -32,122 +41,25 @@ viewport
     .wheel()
     .decelerate()
 
-function assert(condition, message) {
-    if (!condition) {
-        if (message) {
-            throw message;
-        } else {
-            throw "Assertion failed";
-        }
-    }
-}
+viewport.fit();
+viewport.moveCenter(WORLD_WIDTH / 2, WORLD_HEIGHT / 2);
+
+let spritesheet = await PIXI.Assets.load("/static/images/assets.json");
 
 class Tile {
-    size = 370;
-
-    texture_prefix = "/static/assets/catan";
-    texture_map = {
-        "clay": this.texture_prefix + "/tile_brick-resources.assets-408.png",
-        "desert": this.texture_prefix + "/tile_desert-resources.assets-494.png",
-        "wheat": this.texture_prefix + "/tile_grain-resources.assets-1014.png",
-        "wood": this.texture_prefix + "/tile_lumber-resources.assets-1336.png",
-        "ore": this.texture_prefix + "/tile_ore-resources.assets-1349.png",
-        "water": this.texture_prefix + "/tile_water-resources.assets-1219.png",
-        "sheep": this.texture_prefix + "/tile_wool-resources.assets-418.png"
-    }
-
     constructor(viewport, type) {
         this.viewport = viewport;
         this.type = type;
 
-        assert(type in this.texture_map);
-        this.sprite = PIXI.Sprite.from(this.texture_map[type]);
-        this.sprite.anchor.set(0.5);
-
-        this.mask = new Graphics();
-        this.mask.beginFill(0xffffff);
-        //this.mask.drawRoundedPolygon(0, 0, 210, 6, 4)
-        this.mask.drawRegularPolygon(0, 0, 214, 6);
-        this.mask.endFill();
-
-        this.container = new PIXI.Container();
-        this.container.mask = this.mask;
-        this.container.addChild(this.sprite);
-        this.container.position.set(0, 0);
+        let type_img = this.type + ".png";
+        this.sprite = new Sprite(spritesheet.textures[type_img]);
     }
 
-    place(from_tile, side) {
-        this.viewport.addChild(this.mask);
-        this.viewport.addChild(this.container);
-
-        let x = 0;
-        let y = 0;
-        if (from_tile) {
-            let from_x = from_tile.container.position.x;
-            let from_y = from_tile.container.position.y;
-
-            switch (side) {
-                case '0':
-                    x = from_x + this.size;
-                    y = from_y;
-                    break;
-                case '1':
-                    x = from_x + (this.size / 2);
-                    y = from_y + (this.size * 7 / 8) - 3;
-                    break;
-                case '2':
-                    x = from_x - (this.size / 2);
-                    y = from_y + (this.size * 7 / 8) - 3;
-                    break;
-                case '3':
-                    x = from_x - this.size;
-                    y = from_y;
-                    break;
-                case '4':
-                    x = from_x - (this.size / 2);
-                    y = from_y - (this.size * 7 / 8) + 3;
-                    break;
-                case '5':
-                    x = from_x + (this.size / 2);
-                    y = from_y - (this.size * 7 / 8) + 3
-                    break;
-                default:
-                    assert(false);
-                    break;
-            }
-        }
-
-        this.mask.position.set(x, y);
-        this.container.position.set(x, y);
+    place(x, y) {
+        this.viewport.addChild(this.sprite);
+        this.sprite.position.set(0, 0);
     }
 }
 
-function create_board(board, tile_set) {
-    tile_set = arrayShuffle([...tile_set]);
-
-    let start_tile = new Tile(viewport, tile_set.pop());
-    start_tile.place(null, null);
-    let tiles = {
-        0: start_tile
-    }
-
-    let tile_idx_queue = [0]
-    while (tile_idx_queue.length > 0) {
-        let current_idx = tile_idx_queue.shift();
-        let current_tile = tiles[current_idx];
-
-        let tile_definition = board[current_idx];
-        for (let side in tile_definition.neighbors) {
-            let neighbor_idx = tile_definition.neighbors[side];
-            if (!(neighbor_idx in tiles)) {
-                tile_idx_queue.push(neighbor_idx);
-
-                let new_tile = new Tile(viewport, tile_set.pop());
-                new_tile.place(current_tile, side);
-                tiles[neighbor_idx] = new_tile;
-            }
-        }
-    }
-}
-
-create_board(standard_board_definition, standard_tile_set);
+let tile = new Tile(viewport, "sheep");
+tile.place(0, 0);
