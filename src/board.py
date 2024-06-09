16077016d7c8fd7d3d405fad7b75003078bf0fc8
Sam Clark <3758302+goatgoose@users.noreply.github.com>
2024-06-09 18:16:37 -0400

    tile neighbor graph

--- src/board.py
+++ src/board.py
@@ -1,7 +1,8 @@
 from enum import Enum, auto
-from typing import Tuple, Dict, Optional, Set
+from typing import Tuple, Dict, Optional, Set, List
 import uuid
 from abc import ABC
+import pprint
 
 
 class BoardItem(ABC):
@@ -17,22 +18,6 @@ class TileType(Enum):
     WOOD = "wood"
     DESERT = "desert"
     UNKNOWN = "unknown"
-
-
-class Tile(BoardItem):
-    def __init__(self,type_: TileType):
-        self.type = type_
-
-        self.edges: Dict[TileSide, Edge] = {}
-
-        super().__init__()
-
-
-class Intersection(BoardItem):
-    def __init__(self):
-        self.edges: [Edge] = []
-
-        super().__init__()
 
 
 class TileSide(Enum):
@@ -57,6 +42,46 @@ class TileSide(Enum):
     @staticmethod
     def wrap(side: int):
         return TileSide(side % len(TileSide))
+
+
+class Tile(BoardItem):
+    def __init__(self, type_: TileType):
+        self.type = type_
+
+        self.neighbors: Dict[TileSide, Tile] = {}
+        self.edges: Dict[TileSide, Edge] = {}
+        self.intersections: Dict[TileSide, Intersection] = {}
+
+        super().__init__()
+
+    def bfs(self):
+        visited = set()
+        tiles = [self]
+
+        while len(tiles) > 0:
+            tile = tiles.pop(0)
+            if tile in visited:
+                continue
+
+            visited.add(tile)
+            for neighbor in self.neighbors.values():
+                if neighbor not in visited:
+                    tiles.append(neighbor)
+
+            yield tile
+
+    def __str__(self):
+        return self.id
+
+    def __repr__(self):
+        return str(self)
+
+
+class Intersection(BoardItem):
+    def __init__(self):
+        self.edges: Set[Edge] = set()
+
+        super().__init__()
 
 
 class Edge(BoardItem):
@@ -100,40 +125,63 @@ class Board:
             tile = tiles.pop(0)
             board.tiles[tile.id] = tile
 
-            if tile_depth[tile] >= radius:
-                continue
+            # create new neighboring tiles
+            if tile_depth[tile] < radius:
+                for tile_side in TileSide:
+                    if tile_side in tile.neighbors:
+                        continue
 
-            # create a new neighboring tile for each empty border
+                    new_tile = Tile(TileType.UNKNOWN)
+                    board.tiles[new_tile.id] = new_tile
+                    tile_depth[new_tile] = tile_depth[tile] + 1
+                    tiles.append(new_tile)
+
+                    tile.neighbors[tile_side] = new_tile
+                    new_tile.neighbors[TileSide.opposite(tile_side)] = tile
+
+            # associate neighboring tiles with each other
             for tile_side in TileSide:
-                if tile_side in tile.edges:
+                tile_1 = tile.neighbors.get(tile_side)
+                if not tile_1:
+                    continue
+                tile_2 = tile.neighbors.get(TileSide.wrap(tile_side.value + 1))
+                if not tile_2:
                     continue
 
-                new_tile = Tile(TileType.UNKNOWN)
-                board.tiles[new_tile.id] = new_tile
-                tiles.append(new_tile)
-                tile_depth[new_tile] = tile_depth[tile] + 1
+                tile_1_side, tile_2_side = {
+                    TileSide.NORTH: (TileSide.SOUTH_EAST, TileSide.NORTH_WEST),
+                    TileSide.NORTH_EAST: (TileSide.SOUTH, TileSide.NORTH),
+                    TileSide.SOUTH_EAST: (TileSide.SOUTH_WEST, TileSide.NORTH_EAST),
+                    TileSide.SOUTH: (TileSide.NORTH_WEST, TileSide.SOUTH_EAST),
+                    TileSide.SOUTH_WEST: (TileSide.NORTH, TileSide.SOUTH),
+                    TileSide.NORTH_WEST: (TileSide.NORTH_EAST, TileSide.SOUTH_WEST),
+                }.get(tile_side)
 
-                neighboring_edge = Edge()
-                neighboring_edge.associate_tile(tile)
-                neighboring_edge.associate_tile(new_tile)
+                tile_1.neighbors[tile_1_side] = tile_2
+                tile_2.neighbors[tile_2_side] = tile_1
 
-                tile.edges[tile_side] = neighboring_edge
-                new_tile.edges[TileSide.opposite(tile_side)] = neighboring_edge
-
-            # connect each edge to its intersection
-            for tile_side in TileSide:
-                edge_1 = tile.edges[tile_side]
-                edge_2 = tile.edges[TileSide.wrap(tile_side.value + 1)]
-
-                if edge_1.f
-
-            # connect each neighboring tile to its new neighbor
-            for tile_side in TileSide:
-                neighbor_1 = tile.edges[tile_side].other_tile(tile)
-                neighbor_2 = tile.edges[TileSide.wrap(tile_side.value + 1)].other_tile(tile)
-
-
-
+        return board
 
     def to_json(self):
         pass
+
+    def tile_graph(self):
+        graph: Dict[Tile, List[Tile]] = {}
+        for tile in self.tiles.values():
+            graph[tile] = list(tile.neighbors.values())
+        return graph
+
+    def __str__(self):
+        return f"""
+        len: {len(self.tiles)}
+        """
+
+    def __repr__(self):
+        return str(self)
+
+
+if __name__ == '__main__':
+    board = Board.from_radius(2)
+    print(board)
+
+    pprint.pprint(board.tile_graph())
